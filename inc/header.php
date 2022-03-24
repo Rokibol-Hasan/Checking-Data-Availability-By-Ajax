5e36774eeaf2ea4ccf8884dae971af9b4fddbf33
Rokibol-Hasan <rokibul.techdynobd@gmail.com>
2022-03-25 02:31:54 +0600

    CDN updated

--- inc/header.php
+++ inc/header.php
@@ -7,15 +7,13 @@ $pro = new Project();
 ?>
 <!doctype html>
 <html>
-
 <head>
 	<title>Ajax Necessary Projects</title>
 	<link rel="stylesheet" href="css/main.css">
 	<script src="js/jquery.js"></script>
 	<script src="js/main.js"></script>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-
 <body>
 	<section class="header">
 		<div class="container">
